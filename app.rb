4680420b3296e53bafa109f315c4c6ef6152f857
MullaN <admullan@gmail.com>
2020-09-15 10:18:39 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
-
+    post '/team' do
+        @team_name = params[:name]
+        @team_array = [
+            ["Coach", params[:coach]],
+            ["Point Guard", params[:pg]],
+            ["Shooting Guard", params[:sg]],
+            ["Small Forward", params[:sf]],
+            ["Power Forward", params[:pf]],
+            ["Center", params[:c]],
+        ]
+        erb :team
+    end
 end
